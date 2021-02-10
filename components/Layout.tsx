0377161d2e844e14fa3db132552ca64ec70392be
Jay T <jaytula@gmail.com>
2021-02-09 18:23:41 -0800

    Add Layout component and use in _app.js. In 20:44.

---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Layout.module.css";
+
+const Layout: React.FC = ({ children }) => {
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
