4b330fbc87383a20450e3982eb636f2d312597c6
Jay T <jaytula@gmail.com>
2021-02-09 18:10:46 -0800

    Add page by adding file. Head component from /next/head. In 16:05

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import React from "react";
+
+const about = () => {
+  return (
+    <div>
+      <Head>
+        <title>WebDev About</title>
+      </Head>
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default about;
