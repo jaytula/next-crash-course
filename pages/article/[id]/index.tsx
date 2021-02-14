9c4ba276fbd18cf5a86b06eb1b755196e8e095b6
Jay T <jaytula@gmail.com>
2021-02-14 00:25:03 -0800

    Add useRouter example to get [id]. In 42:43

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,5 +1,10 @@
-const index = () => {
-  return <div></div>;
+import { useRouter } from "next/router";
+
+const article = () => {
+  // const router = useRouter();
+  // const { id } = router.query;
+
+  return <div>This is an article {id}</div>;
 };
 
-export default index;
+export default article;
