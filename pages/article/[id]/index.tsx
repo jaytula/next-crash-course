171ea0845520c1b66eb382981479b7762e202458
Jay T <jaytula@gmail.com>
2021-02-15 14:11:28 -0800

    Example use of getServerSideProps. In 46:00

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,10 +1,33 @@
 import { useRouter } from "next/router";
-
-const article = () => {
+import Link from "next/link";
+const article: React.FC<{
+  article: { id: string; title: string; body: string };
+}> = ({ article }) => {
   // const router = useRouter();
   // const { id } = router.query;
 
-  return <div>This is an article {id}</div>;
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.body}</p>
+      <br />
+      <Link href="/">Go Back</Link>
+    </div>
+  );
+};
+
+export const getServerSideProps = async (context) => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+  );
+
+  const article = await res.json();
+
+  return {
+    props: {
+      article,
+    },
+  };
 };
 
 export default article;
