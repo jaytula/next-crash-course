8d2072fa2e5cdf1d9be9d0d6d3f06fd2811608b0
Jay T <jaytula@gmail.com>
2021-02-20 07:54:25 -0800

    Use /api/articles endopint in pages/index.js vis getStaticProps. In 1:02:02

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import ArticleList from "../components/ArticleList";
+import {server} from '../config'
 
 export default function Home({ articles }) {
   return (
@@ -13,9 +14,21 @@ export default function Home({ articles }) {
   );
 }
 
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//   );
+//   const articles = await res.json();
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
+
 export const getStaticProps = async () => {
   const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+    `${server}/api/articles`
   );
   const articles = await res.json();
   return {
